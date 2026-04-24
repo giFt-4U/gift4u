b7e96ab7b3c6b92da7d8888577a5a4900a9c845e
LHI020912 <gpdlsrhd@gmail.com>
2026-04-24 19:01:34 +0900

    chore: F4 backend 초기 구조 생성

---
+++ backend/gift-4u/src/main/java/com/gift_4u/F4/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gift_4u.F4;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Gift4uApplication.class);
+	}
+
+}
---
+++ backend/gift-4u/src/test/java/com/gift_4u/F4/Gift4uApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gift_4u.F4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gift4uApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
