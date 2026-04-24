09eb506cfdef6130ec80b7bb4af06fd96e60248e
LHI020912 <gpdlsrhd@gmail.com>
2026-04-24 18:55:40 +0900

    chore: F4 project 초기 구조 생성

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+// 라우터 설정 파일
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>따숨품 프로젝트</h1>
+    </div>
+  )
+}
+
+export default App
