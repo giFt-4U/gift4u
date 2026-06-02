d30b7459684648b7675ee4c540495ed2817415fb
LHI020912 <gpdlsrhd@gmail.com>
2026-06-02 14:49:00 +0900

    feat: FE chatList/ChatAddFriend/chatRoom

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -17,7 +17,7 @@ const axiosInstance = axios.create({
  */
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
-    if (tonken) {
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
