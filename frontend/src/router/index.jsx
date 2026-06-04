a4ff6853ff4cfe21676e2bcf17a66d6d6b837d0a
윤 재 원 <wodnjs312391@gmail.com>
2026-06-04 16:55:57 +0900

    feat 라우터 수정

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,65 +1,77 @@
-//index.jsx
+// index.jsx
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from '../App';
+
 import Home from '../pages/Home';
 import ProductPage from '../pages/ProductPage';
 import ProductDetail from '../pages/ProductDetail';
 import SearchPage from '../pages/SearchPage';
+
 import CartPage from '../pages/CartPage';
 import OrderPage from '../pages/OrderPage';
 import WishlistPage from '../pages/WishlistPage';
 
+import ChatAddFriend from '../pages/chat/ChatAddFriend';
+import ChatList from '../pages/chat/ChatList';
+import ChatRoom from '../pages/chat/ChatRoom';
+
+import GiftCard from '../pages/gift/GiftCard';
+import GiftCardView from '../pages/gift/GiftCardView';
+import GiftAddress from '../pages/gift/GiftAddress';
+import GiftAccept from '../pages/gift/GiftAccept';
+
+import FriendRequestList from '../pages/friend/FriendRequstList';
+
+import LoginPage from '../pages/user/LoginPage';
+import SignupPage from '../pages/user/SignupPage';
+import KakaoCallbackPage from '../pages/user/KakaoCallbackPage';
+import MyPage from '../pages/user/MyPage';
+import FriendsPage from '../pages/user/FriendsPage';
+
 export default function Router() {
-
     return (
-
         <BrowserRouter>
-
             <Routes>
+                <Route path="/" element={<App />}>
+                    <Route index element={<Home />} />
 
-                <Route
-                    path="/"
-                    element={<App />}
-                >
+                    {/* PRODUCT */}
+                    <Route path="products" element={<ProductPage />} />
+                    <Route path="products/:id" element={<ProductDetail />} />
 
-                    <Route
-                        index
-                        element={<Home />}
-                    />
+                    {/* SEARCH */}
+                    <Route path="search" element={<SearchPage />} />
 
-                    <Route
-                        path="products"
-                        element={<ProductPage />}
-                    />
+                    {/* CART / ORDER / WISHLIST */}
+                    <Route path="cart" element={<CartPage />} />
+                    <Route path="order" element={<OrderPage />} />
+                    <Route path="wishlist" element={<WishlistPage />} />
 
-                    <Route
-                        path="products/:id"
-                        element={<ProductDetail />}
-                    />
+                    {/* CHAT */}
+                    <Route path="chat" element={<ChatList />} />
+                    <Route path="chat/add" element={<ChatAddFriend />} />
+                    <Route path="chat/:roomId" element={<ChatRoom />} />
 
-                    <Route
-                        path="search"
-                        element={<SearchPage />}
-                    />
-                    <Route
-                        path="cart"
-                        element={<CartPage />}
-                    />
-                    <Route
-                        path="order"
-                        element={<OrderPage />}
-                    />
-                    <Route
-                        path="wishlist"
-                        element={<WishlistPage />}
-                    />
+                    {/* GIFT */}
+                    <Route path="gifts/card" element={<GiftCard />} />
+                    <Route path="gifts/card/preview" element={<GiftCardView />} />
+                    <Route path="gifts/:uuid" element={<GiftCardView />} />
+                    <Route path="gifts/:uuid/address" element={<GiftAddress />} />
+                    <Route path="gifts/:uuid/accept" element={<GiftAccept />} />
 
+                    {/* AUTH */}
+                    <Route path="login" element={<LoginPage />} />
+                    <Route path="signup" element={<SignupPage />} />
+                    <Route path="kakao/auth-code" element={<KakaoCallbackPage />} />
+                    <Route path="mypage" element={<MyPage />} />
+                    <Route path="friends" element={<FriendsPage />} />
+
+                    {/* FRIEND */}
+                    <Route path="friends/requestlist" element={<FriendRequestList />} />
                 </Route>
-
             </Routes>
-
         </BrowserRouter>
     );
-
 }
